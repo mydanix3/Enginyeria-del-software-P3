0c0b5c69a2ae520ed9440036382fcb5fd2db6bfa
Didac Altesa <didacaltesa@gmail.com>
2020-05-15 20:21:24 +0200

    Update PaymentData

--- PaymentData.py
+++ PaymentData.py
@@ -1,16 +1,11 @@
 class PaymentData:
-
-def __init__(self,tipustargeta,nom,code,import):
-
-    self.TipusTargeta=tipustargeta
-    self.NomTitular=nom
-    self.CodiSeguretat=code
-    self.Import=import
-
     TipusTargeta=None
     NomTitular=None
     CodiSeguretat=None
     Import=None
+    
+def __init__(self):
 
-    def __init__(self):
-        pass
+    pass
+
+    

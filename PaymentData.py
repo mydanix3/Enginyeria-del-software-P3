92db1e441922749f8fb3718445925bac47f7d406
shirkpsi <65337879+shirkpsi@users.noreply.github.com>
2020-05-15 20:17:53 +0200

    Update PaymentData.py
    Classe Payment data

--- PaymentData.py
+++ PaymentData.py
@@ -1,4 +1,16 @@
 class PaymentData:
 
+def __init__(self,tipustargeta,nom,code,import):
+
+    self.TipusTargeta=tipustargeta
+    self.NomTitular=nom
+    self.CodiSeguretat=code
+    self.Import=import
+
+    TipusTargeta=None
+    NomTitular=None
+    CodiSeguretat=None
+    Import=None
+
     def __init__(self):
         pass

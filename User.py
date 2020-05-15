6d4d2876d8fa7914f0d6b2dfbc44aef8e07a4aaf
Didac Altesa <didacaltesa@gmail.com>
2020-05-15 19:31:11 +0200

    Atributs Classe User

--- User.py
+++ User.py
@@ -1,4 +1,6 @@
-class User:
-
-    def __init__(self):
+class User:		
+    def __init__(self,dni,email,phonenumber):
+    	self.DNI = dni
+    	self.Email = email
+    	self.PhoneNumber = phonenumber
         pass
